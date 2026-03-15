3e821efb5c6473a5bb3d5e276f5ff26166c417c4
andersongonzalez0602-svg <andersongonzalez0602@gmail.com>
2026-03-15 11:18:41 -0400

    l

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 import './i18n'
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 )
